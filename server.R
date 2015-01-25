accfcc239f744a7952a0a87ab4206053e8072327
gcallao <callao.gc@gmail.com>
2015-01-25 18:27:13 -0500

    Updating

--- server.R
+++ server.R
@@ -15,35 +15,102 @@ train$Sex <- factor(train$Sex); test$Sex <- factor(test$Sex); dataQNB$Sex <- fac
 train$Embarked <- factor(train$Embarked); test$Embarked <- factor(test$Embarked); dataQNB$Embarked <- factor(dataQNB$Embarked)
 train$FamilySize <- train$SibSp + train$Parch + 1; test$FamilySize <- test$SibSp + test$Parch + 1
 
-fit <- rpart(Survived ~ Pclass + Sex + Age + Fare + Embarked + FamilySize, data = train, method = "class")
 
-prediction_survival <- predict(fit, dataQNB, type = "class")
-levels(prediction_survival) <- c("Die", "Survive")
+fit.default <- rpart(Survived ~ Pclass + Sex + Age + Fare + Embarked + FamilySize, data = train, method = "class")
 
-prediction_probs <- predict(fit, dataQNB)
+fit.low <- rpart(Survived ~ Pclass + Sex + Age + Fare + Embarked + FamilySize, data = train, 
+                     method = "class", control=rpart.control(minsplit=200)) 
 
-prediction_probabilities <- c()
+fit.high  <- rpart(Survived ~ Pclass + Sex + Age + Fare + Embarked + FamilySize, data = train, 
+                        method = "class", control=rpart.control(cp=0))          
+  
+prediction_survival.default <- predict(fit.default, dataQNB, type = "class")
+prediction_survival.low <- predict(fit.low, dataQNB, type = "class")
+prediction_survival.high <- predict(fit.high, dataQNB, type = "class")
 
-for (i in c(1:dim(prediction_probs)[1])){
-        if(prediction_probs[i, 1] > prediction_probs[i, 2]){
-                prediction_probabilities <- append(prediction_probabilities, prediction_probs[i, 1])
+levels(prediction_survival.default) <- c("Die", "Survive")
+levels(prediction_survival.low) <- c("Die", "Survive")
+levels(prediction_survival.high) <- c("Die", "Survive")
+
+prediction_probs.default <- predict(fit.default, dataQNB)
+prediction_probs.low <- predict(fit.low, dataQNB)
+prediction_probs.high <- predict(fit.high, dataQNB)
+
+prediction_probabilities.default <- c()
+prediction_probabilities.low <- c()
+prediction_probabilities.high <- c()
+
+for (i in c(1:dim(prediction_probs.default)[1])){
+        if(prediction_probs.default[i, 1] > prediction_probs.default[i, 2]){
+                prediction_probabilities.default <- append(prediction_probabilities.default, prediction_probs.default[i, 1])
         }else{
-                prediction_probabilities <- append(prediction_probabilities, prediction_probs[i, 2])
+                prediction_probabilities.default <- append(prediction_probabilities.default, prediction_probs.default[i, 2])
         }      
 }
 
-prediction_table <- data.frame(Names = dataQNB[,2], Outcome = prediction_survival, Probability = prediction_probabilities)
+for (i in c(1:dim(prediction_probs.low)[1])){
+        if(prediction_probs.low[i, 1] > prediction_probs.low[i, 2]){
+                prediction_probabilities.low <- append(prediction_probabilities.low, prediction_probs.low[i, 1])
+        }else{
+                prediction_probabilities.low <- append(prediction_probabilities.low, prediction_probs.low[i, 2])
+        }      
+}
 
-predictionTest <- predict(fit, test, type = "class")
-CfMatrix <- confusionMatrix(predictionTest, test$Survived)
+for (i in c(1:dim(prediction_probs.high)[1])){
+        if(prediction_probs.high[i, 1] > prediction_probs.high[i, 2]){
+                prediction_probabilities.high <- append(prediction_probabilities.high, prediction_probs.high[i, 1])
+        }else{
+                prediction_probabilities.high <- append(prediction_probabilities.high, prediction_probs.high[i, 2])
+        }      
+}
+
+prediction_table.default <- data.frame(Names = dataQNB[,2], Outcome = prediction_survival.default, Probability = prediction_probabilities.default)
+prediction_table.low <- data.frame(Names = dataQNB[,2], Outcome = prediction_survival.low, Probability = prediction_probabilities.low)
+prediction_table.high <- data.frame(Names = dataQNB[,2], Outcome = prediction_survival.high, Probability = prediction_probabilities.high)
+
+predictionTest.default <- predict(fit.default, test, type = "class")
+predictionTest.low <- predict(fit.low, test, type = "class")
+predictionTest.high <- predict(fit.high, test, type = "class")
+
+CfMatrix.default <- confusionMatrix(predictionTest.default, test$Survived)
+CfMatrix.low <- confusionMatrix(predictionTest.low, test$Survived)
+CfMatrix.high <- confusionMatrix(predictionTest.high, test$Survived)
 
 shinyServer(function(input, output){
-        
-        output$plot <- renderPlot(fancyRpartPlot(fit))
+
+        output$plot <- renderPlot(if(input$select == "Default"){
+                        fancyRpartPlot(fit.default)
+                        }else{
+                                if(input$select == "Low"){
+                                    fancyRpartPlot(fit.low)  
+                                }else{
+                                        fancyRpartPlot(fit.high)  
+                                }
+                        }
+                        )
         
         output$dataQNB <- renderDataTable({dataQNB}, options = list(lengthMenu = c(5, 30, 50), pageLength = 5))
         
-        output$Prediction <- renderDataTable({prediction_table}, options = list(lengthMenu = c(5, 30, 50), pageLength = 5))
+        output$Prediction <- renderDataTable({if(input$select == "Default"){
+                                prediction_table.default
+                                }else{
+                                        if(input$select == "Low"){
+                                                prediction_table.low    
+                                        } else {
+                                                prediction_table.high       
+                                        }
+                                }
         
-        output$Accuracy <- renderPrint(CfMatrix)
+        }, options = list(lengthMenu = c(5, 30, 50), pageLength = 5))
+        
+        output$Accuracy <- renderPrint(if(input$select == "Default"){
+                           CfMatrix.default
+                        }else{
+                                if(input$select == "Low"){
+                                        CfMatrix.low  
+                                }else{
+                                        CfMatrix.high  
+                                }
+                        }
+                )
 })
--- ui.R
+++ ui.R
@@ -12,13 +12,18 @@ shinyUI(fluidPage(
                            "by John P. Eaton, at Queenstown (now Cobh) in addition to the names of 115 adults and 5 children who boarded, there also
                             appear on the list names 18 people who did not use the third-class tickets they had purchased for passage aboard the Titanic."),
                         
-                        h5("In this App we are going to explore through a prediction model based on decision trees, what would happened 
+                        h5("In this App we are going to explore through prediction models based on decision trees, what would happened 
                            if these people had indeed been aboard the ship."),
                         
-                        h5("The data used to train the model and to obtain the perfomance indicators belong to the famous competition at 
+                        h5("The data used to train the models and to obtain the perfomance indicators belong to the famous competition at 
                            Kaggle, ", a("Titanic: Machine Learning from Disaster", href = "https://www.kaggle.com/c/titanic-gettingStarted")),
+
+                        selectInput("select", label = h6("Decision Tree Complexity"),
+                                    c("Default", "Low", "High"), selected = "Default"),
                         
                         h6("App developed by ", a("Giancarlo Callaoapaza", href = "https://www.linkedin.com/in/gcallaoapaza/en"), "(2015).")
+                        
+
                 ),
                 
                 mainPanel(
